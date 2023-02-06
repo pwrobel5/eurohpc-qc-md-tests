refactor: moved some methods to classes, corrected bug with calculation of rotation

systems-xyz/extend_cell.py
@@ -1,7 +1,5 @@
 import argparse
-from utils import System, Atom
-
-import numpy as np
+from utils import read_system_from_xyz
 
 parser = argparse.ArgumentParser(description='Create .xyz file with extended cell along chosen vectors from basic .xyz file')
 
@@ -18,16 +16,7 @@ cell_vector = args.cell
 times = args.times
 output_file_name = args.output_name
 
-with open(input_file_path, 'r') as input_file:
-    atoms_number = int(input_file.readline())
-    input_file.readline()
-    atoms_list = []
-    for _ in range(atoms_number):
-        line = input_file.readline().split()
-        current_atom = Atom(line[0], np.array([float(line[1]), float(line[2]), float(line[3])]))
-        atoms_list.append(current_atom)
-
-atomic_system = System(np.array(cell_vector), atoms_list)
+atomic_system = read_system_from_xyz(input_file_path, cell_vector)
 for direction in directions:
     atomic_system = atomic_system.multiple_in_direction(direction, times)
 

systems-xyz/standarize_cell.py
@@ -1,7 +1,6 @@
-import numpy as np
 import argparse
 
-from utils import Atom
+from utils import read_system_from_xyz
 
 parser = argparse.ArgumentParser(description='Center coordinates at the position of given atom, rotate to have X axis at desired direction')
 
@@ -16,27 +15,9 @@ center_index = args.center_index
 pair_indices = args.pair_indices
 output_file_name = args.output_name
 
-atoms_list = []
-with open(input_file_path, 'r') as input_file:
-    atoms_number = int(input_file.readline())
-    commentary = input_file.readline()
-    
-    for _ in range(atoms_number):
-        line = input_file.readline().split()
-        atoms_list.append(Atom(line[0], np.array([float(line[1]), float(line[2]), float(line[3])])))
-
-center_position = atoms_list[center_index].coordinates
-moving_vector = -1.0 * center_position
-centered_atoms = [x.move_by_vector(moving_vector) for x in atoms_list]
-
-current_x_vector = np.array([1.0, 0.0, 0.0])
-new_x_vector = centered_atoms[pair_indices[1]].coordinates - centered_atoms[pair_indices[0]].coordinates
-new_x_vector[-1] = 0.0  # make the z coordinate equal 0, as here only rotation in xy plane is considered
-vector_product = current_x_vector @ new_x_vector
-angle = np.arccos(vector_product / np.linalg.norm(new_x_vector))  # divide only by norm of the second vector as the first is equal 1
-rotated_atoms = [x.rotate_by_angle(angle) for x in centered_atoms]
+atom_system = read_system_from_xyz(input_file_path, include_commentary=True)
+centered_system = atom_system.center_at_position(center_index)
+rotated_system = centered_system.place_atoms_at_x_axis(pair_indices)
 
 with open(output_file_name, 'w') as output_file:
-    output_file.write('{}\n'.format(atoms_number))
-    output_file.write(commentary)
-    output_file.writelines('\n'.join([str(a) for a in rotated_atoms]))
+    output_file.write(str(rotated_system))

systems-xyz/utils/atom.py
@@ -15,9 +15,11 @@ class Atom:
         return moved_atom
     
     def rotate_by_angle(self, angle: float) -> Atom:
-        rotation_matrix = np.array([[np.cos(angle), -np.sin(angle), 0.0],
-                                    [np.sin(angle),  np.cos(angle), 0.0],
-                                    [          0.0,            0.0, 1.0]])
+        # rotation of coordination system is made counter-clockwise by a given angle
+        # thus rotation of vector coordinates is clockwise
+        rotation_matrix = np.array([[ np.cos(angle), np.sin(angle), 0.0],
+                                    [-np.sin(angle), np.cos(angle), 0.0],
+                                    [           0.0,           0.0, 1.0]])
         new_coordinates = rotation_matrix @ self._coordinates
         return Atom(self._symbol, new_coordinates)
     

systems-xyz/utils/system.py
@@ -12,7 +12,7 @@ DIRECTION_TO_INDEX = {
 
 
 class System:
-    def __init__(self, cell_vectors: npt.ArrayLike, atoms: list[Atom]) -> System:
+    def __init__(self, cell_vectors: npt.ArrayLike, atoms: list[Atom], commentary: str='') -> System:
         length = cell_vectors.shape[0]
         if length == 1:
             self._cell_vectors = np.array([cell_vectors[0], cell_vectors[0], cell_vectors[0]])
@@ -21,6 +21,7 @@ class System:
         else:
             self._cell_vectors = cell_vectors
         self._atoms = atoms
+        self._commentary = commentary.strip()
     
     def multiple_in_direction(self, direction: str, times: int) -> System:
         index = DIRECTION_TO_INDEX[direction]
@@ -39,9 +40,28 @@ class System:
         new_system = System(new_cell_vector, self._atoms + moved_atoms)
         return new_system
     
+    def center_at_position(self, center_index: int) -> System:
+        center_position = self._atoms[center_index].coordinates
+        moving_vector = -1.0 * center_position
+        centered_atoms = [x.move_by_vector(moving_vector) for x in self._atoms]
+        return System(self._cell_vectors, centered_atoms, self._commentary)
+    
+    def place_atoms_at_x_axis(self, pair_indices: list[int]) -> System:
+        new_x_vector = self._atoms[pair_indices[1]].coordinates - self._atoms[pair_indices[0]].coordinates
+        angle = np.arctan2(new_x_vector[1], new_x_vector[0])  # using atan2 to obtain the correct orientation
+        rotated_atoms = [x.rotate_by_angle(angle) for x in self._atoms]
+        return System(self._cell_vectors, rotated_atoms, self._commentary)
+    
     def __str__(self) -> str:
         lines = []
         lines.append('{}'.format(len(self._atoms)))
-        lines.append('cell {:6.3f} {:6.3f} {:6.3f}'.format(self._cell_vectors[0], self._cell_vectors[1], self._cell_vectors[2]))
+        if len(self._commentary) > 0:
+            lines.append(self._commentary)
+        else:
+            lines.append('cell {:6.3f} {:6.3f} {:6.3f}'.format(self._cell_vectors[0], self._cell_vectors[1], self._cell_vectors[2]))
         lines.extend([str(x) for x in self._atoms])
         return '\n'.join(lines)
+    
+    @property
+    def periodic(self) -> bool:
+        return self._cell_vectors.size == 0